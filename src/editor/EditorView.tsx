d895cd1898943840a7928fb975d3c7db163c8f10
Michał Kardyś <michal@kardys.dev>
2021-09-06 16:54:00 +0200

    feat: add id generation

--- src/editor/EditorView.tsx
+++ src/editor/EditorView.tsx
@@ -1,19 +1,39 @@
 import { useCallback, useState } from "react"
+import { v4 as uuidv4, validate } from 'uuid'
 
-interface Element{}
+type Id = string & {readonly type: symbol}
+
+function generateId() {
+  const newId = uuidv4();
+  isId(newId)
+  return newId;
+}
+function isId(x: string): asserts x is Id{
+  if (!validate(x)){
+    throw new Error('id is not uuid')
+  }
+}
+
+interface Element{
+  id: Id
+}
 
 const Elements = (props: {elements: Element[]}) => {
   const {elements} = props;
 
-  return elements.map((el, index) => <figure><span>element {index}</span></figure>)
+  return <>{
+    elements.map((el, index) => <figure><span>element {el.id}</span></figure>)
+  }</>
 }
 
 export const EditorView = () => {
   const [elements,setElements] = useState<Element[]>([])
   const addElement = useCallback(() => {
-    const nextElement = {}
+    const nextElement = {
+      id: generateId()
+    }
     setElements([...elements, nextElement])
-  },[elements, setElements])
+  },[elements, setElements]);
 
   return <>
     <button onClick={addElement}>Add</button>
