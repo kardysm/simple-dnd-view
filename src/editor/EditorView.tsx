0e903e45078505b7fe4ee71ac01a8c69e0a94032
Michał Kardyś <michal@kardys.dev>
2021-09-06 16:37:52 +0200

    feat: implement adding elements to editor

--- src/editor/EditorView.tsx
+++ src/editor/EditorView.tsx
@@ -1,4 +1,22 @@
+import { useCallback, useState } from "react"
+
+interface Element{}
+
+const Elements = (props: {elements: Element[]}) => {
+  const {elements} = props;
+
+  return elements.map((el, index) => <figure><span>element {index}</span></figure>)
+}
+
 export const EditorView = () => {
+  const [elements,setElements] = useState<Element[]>([])
+  const addElement = useCallback(() => {
+    const nextElement = {}
+    setElements([...elements, nextElement])
+  },[elements, setElements])
+
   return <>
+    <button onClick={addElement}>Add</button>
+    <Elements elements={elements}/>
   </>
 }
