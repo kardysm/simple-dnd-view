629914f22828e1edd90a0efccb36d7fa4aca603b
Michał Kardyś <michal@kardys.dev>
2021-09-06 16:10:30 +0200

    test: init editor tests

---
+++ src/editor/EditorView.spec.tsx
@@ -0,0 +1,10 @@
+import {fireEvent} from '@testing-library/react'
+
+describe.skip('EditorView',()=>{
+  it('should add element on "add" button click', function () {
+
+  });
+  it('should remove element on "remove" button click', function () {
+
+  });
+})
