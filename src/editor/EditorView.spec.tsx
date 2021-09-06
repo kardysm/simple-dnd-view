9cc9b4de7d2052b8e3d17af0874c600b6eac9349
Michał Kardyś <michal@kardys.dev>
2021-09-06 16:28:03 +0200

    test: add test of adding multiple elements

--- src/editor/EditorView.spec.tsx
+++ src/editor/EditorView.spec.tsx
@@ -20,4 +20,25 @@ describe('EditorView',()=>{
   it.skip('should remove highlighted element on "remove" button click', function () {
 
   });
+
+  it.skip('should add 5 elements', async function () {
+    //arrange
+    const {getByRole, findAllByRole} = render(<EditorView/>)
+
+    //assert
+    const addButton = getByRole('button', {name: 'Add'})
+
+    //act
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+
+    //assert
+    const elements = await findAllByRole('figure')
+    expect(elements.length).toBe(5)
+  });
+
+
 })
